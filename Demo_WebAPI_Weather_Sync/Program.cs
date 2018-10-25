e45371edfdd63e6cc37e793b15baa17071cc6078
hanse174 <hanse174@JB202-L21-W-D>
2018-10-25 14:54:56 -0400

    Included API call returning XML

--- Demo_WebAPI_Weather_Sync/Program.cs
+++ Demo_WebAPI_Weather_Sync/Program.cs
@@ -8,6 +8,9 @@ using System.Web;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using System.Net.Http;
+using System.Xml.Serialization;
+using System.Xml.Linq;
+using System.Xml;
 
 namespace Demo_WebAPI_Weather
 {
@@ -34,7 +37,9 @@ namespace Demo_WebAPI_Weather
                 Console.WriteLine();
                 Console.WriteLine("1) Set the Location");
                 Console.WriteLine("2) Display the Current Weather");
-                Console.WriteLine("3) Exit");
+                Console.WriteLine("3) Display weather for a specific city");
+                Console.WriteLine("4) Display weather for a specific zipcode");
+                Console.WriteLine("5) Exit");
                 Console.WriteLine();
                 Console.Write("Enter Choice:");
                 string userMenuChoice = Console.ReadLine();
@@ -50,6 +55,14 @@ namespace Demo_WebAPI_Weather
                         break;
 
                     case "3":
+                        DisplayWeatherByCity();
+                        break;
+
+                    case "4":
+                        DisplayWeatherByZip();
+                        break;
+
+                    case "5":
                         quit = true;
                         break;
 
@@ -59,6 +72,140 @@ namespace Demo_WebAPI_Weather
                 }
             }
         }
+
+
+        static void DisplayWeatherByCity()
+        {
+            string url;
+            string city = "";
+
+            DisplayHeader("Get weather by city");
+
+            // Prompt user for desired city
+            Console.Write("Enter City: ");
+            city = (Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+            sb.Append("http://api.openweathermap.org/data/2.5/weather?");
+            sb.Append("q=" + city);
+            sb.Append("&appid=63dd8a59e9d07448ddaf0cabe2745f6c");
+
+            url = sb.ToString();
+
+            WeatherData currentWeather = new WeatherData();
+
+            currentWeather = HttpGetCurrentWeatherByLocation(url, "json");
+
+            // Clear the console
+            Console.Clear();
+
+            // Display the temp in Fahrenheit
+            Console.WriteLine(ConvertToFahrenheit(currentWeather.Main.Temp));
+
+            DisplayContinuePrompt();
+        }
+
+
+        // Display weather by Zip code -- return XML
+        static void DisplayWeatherByZip()
+        {
+            string url;
+            string zip = "";
+            string result;
+
+            DisplayHeader("Get weather by zip");
+
+            // Prompt user for desired city
+            Console.Write("Enter Zip: ");
+            zip = (Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            sb.Clear();
+            sb.Append("http://api.openweathermap.org/data/2.5/weather?");
+            sb.Append("zip=" + zip);
+            sb.Append("&appid=63dd8a59e9d07448ddaf0cabe2745f6c&mode=xml");
+
+            url = sb.ToString();
+
+            using (WebClient syncClient = new WebClient())
+            {
+                result = syncClient.DownloadString(url);
+            }
+
+            XmlDocument xmltest = new XmlDocument();
+            xmltest.LoadXml(result);
+
+            XmlNodeList elemlist = xmltest.GetElementsByTagName("current");
+
+            string list = elemlist[0].InnerXml;
+
+            Console.WriteLine(list);
+
+
+            //XmlTextReader reader = new XmlTextReader(result);
+            //while (reader.Read())
+            //{
+            //    // Do some work here on the data.
+            //    //Console.WriteLine(reader.Name);
+            //    switch (reader.NodeType)
+            //    {
+            //        case XmlNodeType.Element: // The node is an element.
+            //            Console.Write("<" + reader.Name);
+
+            //            while (reader.MoveToNextAttribute()) // Read the attributes.
+            //                Console.Write(" " + reader.Name + "='" + reader.Value + "'");
+            //            Console.Write(">");
+            //            Console.WriteLine(">");
+            //            break;
+            //        case XmlNodeType.Text: //Display the text in each element.
+            //            Console.WriteLine(reader.Value);
+            //            break;
+            //        case XmlNodeType.EndElement: //Display the end of the element.
+            //            Console.Write("</" + reader.Name);
+            //            Console.WriteLine(">");
+            //            break;
+            //    }
+            //}
+            //Console.ReadLine();
+
+
+            //XmlRootAttribute xRoot = new XmlRootAttribute();
+            //xRoot.IsNullable = true;
+            //xRoot.ElementName = "current";
+            //XmlSerializer serializer = new XmlSerializer(typeof(WeatherData), xRoot);
+            //currentWeather = serializer.Deserialize(result);
+
+            //currentWeather = (WeatherData)serializer.Deserialize(result);
+            //StringReader stringReader = new StringReader(result);
+
+            // currentWeather = (WeatherData)serializer.Deserialize(stringReader);
+            //return currentWeather;
+
+            //WeatherData currentWeather = new WeatherData();
+
+            //currentWeather = HttpGetCurrentWeatherByLocation(url, "xml");
+
+            //XElement e = new XElement("City", "abcde");
+            //Console.WriteLine(e);
+            //Console.WriteLine("Value of e:" + (string)e);
+
+            //IEnumerable<XElement> address =
+            // from el in currentWeather("Weather")
+            //where (string)el.Attribute("Type") == "Billing"
+            //  select el;
+            //foreach (XElement el in address)
+            //    Console.WriteLine(el);
+
+            // Clear the console
+            //Console.Clear();
+
+            // Display the temp in Fahrenheit
+            //Console.WriteLine(currentWeather);
+
+            DisplayContinuePrompt();
+        }
+
 
         static void DisplayOpeningScreen()
         {
@@ -148,20 +295,22 @@ namespace Demo_WebAPI_Weather
             sb.Append("http://api.openweathermap.org/data/2.5/weather?");
             sb.Append("&lat=" + coordinates.Latitude.ToString());
             sb.Append("&lon=" + coordinates.Longitude.ToString());
-            sb.Append("&appid=864d252afc928abff4010abe732617a1");
+            sb.Append("&appid=63dd8a59e9d07448ddaf0cabe2745f6c");
 
             url = sb.ToString();
 
             WeatherData currentWeather = new WeatherData();
  
-            currentWeather = HttpGetCurrentWeatherByLocation(url);
+            currentWeather = HttpGetCurrentWeatherByLocation(url, "json");
 
             return currentWeather;
         }
 
-        static WeatherData HttpGetCurrentWeatherByLocation(string url)
+
+        static WeatherData HttpGetCurrentWeatherByLocation(string url, string mode)
         {
             string result = null;
+            WeatherData currentWeather;
 
             using (WebClient syncClient = new WebClient())
             {
@@ -170,7 +319,32 @@ namespace Demo_WebAPI_Weather
 
             //Console.WriteLine(result);
 
-            WeatherData currentWeather = JsonConvert.DeserializeObject<WeatherData>(result);
+            // Check if response should be xml or json
+            if (mode == "json")
+            {
+                // Return json
+                currentWeather = JsonConvert.DeserializeObject<WeatherData>(result);
+            } else if (mode == "xml")
+            {
+                // Return XMl
+                XmlRootAttribute xRoot = new XmlRootAttribute();
+                xRoot.IsNullable = true;
+                xRoot.ElementName = "current";
+                XmlSerializer serializer = new XmlSerializer(typeof(WeatherData), xRoot);
+                //currentWeather = serializer.Deserialize(result);
+
+                //currentWeather = (WeatherData)serializer.Deserialize(result);
+                StringReader stringReader = new StringReader(result);
+
+                currentWeather = (WeatherData)serializer.Deserialize(stringReader);
+                return currentWeather;
+
+                //StreamReader reader = new StreamReader(path);
+                //cars = (CarCollection)serializer.Deserialize(reader);
+            } else
+            {
+                currentWeather = null;
+            }
 
             return currentWeather;
         }
